ddee23309c28da2c9aa32141e9877c492b79cc52
Bryan Pan <bryanpan342@gmail.com>
2020-10-19 22:52:39 -0500

    firebase basics

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,53 +1,75 @@
 import React, { useState, useEffect } from 'react';
-import firebaseui from 'react-firebaseui';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './styles/App.scss';
+import Home from './Home';
+import Level1 from './Level1';
 
 const config = {
-  apiKey: "AIzaSyARYdiOJuzS_DpdMhWPkunOy2zi1a_oM88",
-  authDomain: "bigspook-7b263.firebaseapp.com",
-  databaseURL: "https://bigspook-7b263.firebaseio.com",
-  projectId: "bigspook-7b263",
-  storageBucket: "bigspook-7b263.appspot.com",
-  messagingSenderId: "636133052717",
-  appId: "1:636133052717:web:ed2cf2142e95d7f71983a4",
-  measurementId: "G-EPQTX4RGJ9"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 firebase.initializeApp(config);
-
-// Configure FirebaseUI.
-const uiConfig = {
-  // Popup signin flow rather than redirect flow.
-  signInFlow: 'popup',
-  // We will display Google and Facebook as auth providers.
-  signInOptions: [
-    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
-  ],
-  callbacks: {
-    // Avoid redirects after sign-in.
-    signInSuccessWithAuthResult: () => false
-  }
-};
-
 
 function App(): JSX.Element {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
-  if(!isSignedIn) {
-    return (
-      <div>
-        <h1>My App</h1>
-        <p>Please sign-in:</p>
+  const uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: () => {
+        setIsSignedIn(true); 
+        return false;
+      }
+    }
+  };
+
+  // Write a button to allow for anonymous sign in?
+  const anonymousSignIn = () => {
+    firebase.auth().signInAnonymously().catch(function(error) {
+      console.error(`Error ${error.code}: ${error.message}`)
+    });
+    
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        setIsSignedIn(true);
+      }
+    });
+  }
+
+  if (!isSignedIn) {
+    return ( 
+      <div id='authWrapper'>
+        <div className='text' id='signIn'> Sign In </div>
         <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
       </div>
     );
   }
-  return(
-    <div>
-      <h1 className='text'>Creative Labs | notpron</h1>
-    </div>
+  return (
+    <Router>
+      <Route exact path="/" render={Home} />
+      <Route
+        exact
+        path="/level1"
+        render={() => <Level1 name="level1" nextLevel="/level2" />}
+      ></Route>
+      <Route
+        exact
+        path="/level2"
+        render={() => <Level1 name="level2" nextLevel="/level3" />}
+      ></Route>
+    </Router>
   );
 }
 
