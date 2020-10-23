5191798add5dd2bf2b0db62b3897c0219fcfa743
Joseph Clegg <josepht.clegg@gmail.com>
2020-10-23 01:02:19 -0700

    Refined rendering functions, swapped some assets, fixed css styling

--- src/components/JLevel.tsx
+++ src/components/JLevel.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import './styles/JLevel.scss';
-import back from '../assets/JLevelAssets/liminalback.png';
-import lhall from '../assets/JLevelAssets/liminaldoorway.png';
-import rhall from '../assets/JLevelAssets/rhall.png';
-import ldoor from '../assets/JLevelAssets/liminaldoor.png';
-import rdoor from '../assets/JLevelAssets/liminaldoorr.png';
+import back from '../assets/JLevelAssets/back.png';
+import lhall from '../assets/JLevelAssets/halll.png';
+import rhall from '../assets/JLevelAssets/hallr.png';
+import ldoor from '../assets/JLevelAssets/doorl.png';
+import rdoor from '../assets/JLevelAssets/doorr.png';
+import ghost from '../assets/JLevelAssets/ghost.png';
 
 interface Space {
   front: string;
@@ -25,19 +26,19 @@ class JLevel extends React.Component {
 }
 
 function RenderLeft(props): JSX.Element {
-  switch(props.sprite) {
-    case d:
+  switch(props.left) {
+    case 'd':
       return <img className='leftImg' src={ldoor} />;
-    case h:
+    case 'h':
       return <img className='leftImg' src={lhall} />;
   }
 }
 
 function RenderRight(props): JSX.Element {
-  switch(props.sprite) {
-    case d:
+  switch(props.right) {
+    case 'd':
       return <img className='rightImg' src={rdoor} />;
-    case h:
+    case 'h':
       return <img className='rightImg' src={rhall} />;
   }
 }
@@ -46,39 +47,52 @@ function RenderFront(): JSX.Element {
   return <img className='back' src={back} />;
 }
 
+function RenderGhost(): JSX.Element {
+  return <img className='leftImg' src={ghost} />;
+}
+
 function RenderSpace(space): JSX.Element {
   return (
-    <div>
-     <RenderFront />
+    <div className='cont2'>
+      <RenderFront />
       {space.left != 'w' && RenderLeft(space.left)}
       {space.right != 'w' && RenderRight(space.right)}
     </div>
   );
 }
 
-function View(props): JSX.Element {
+function RenderHallHelper(spaces): JSX.Element {
+  return (
+    <div className='cont2'>
+      {RenderFront()}
+      {RenderLeft(spaces[0])}
+      {RenderRight(spaces[0])}
+      {(spaces.length != 1) && RenderHallHelper(spaces.slice(1))}
+      {(spaces[0].floor == 'g') && RenderGhost()}
+    </div>
+  );
+}
+
+function RenderHall(spaces): JSX.Element {
   return (
     <div className='imgCont'>
-      <img className='back' src={back} />
-      <div className='cont2'>
-        <img className='back' src={back} />
-	<div className='cont2'>
-	  <img className='back' src={back} />
-	  <img className='leftImg' src={lhall} />
-	  <img className='rightImg' src={rhall} />
-	  <div className='cont2'>
-	    <img className='back' src={back} />
-	    <div className='cont2'>
-	      <img className='back' src={back} />
-	      <img className='leftImg' src={lhall} />
-	      <img className='rightImg' src={rhall} />
-	    </div>
-	  </div>
-	</div>
-      </div>
-      <img className='leftImg' src={lhall} />
-      <img className='rightImg' src={rhall} />
+      {RenderFront()}
+      {RenderLeft(spaces[0])}
+      {RenderRight(spaces[0])}
+      {(spaces.length != 1) && RenderHallHelper(spaces.slice(1))}
+      {(spaces[0].floor == 'g') && RenderGhost()}
     </div>
+  );
+}
+
+function View(): JSX.Element {
+  var space1 = {front:'', left:'h', right:'d', floor:''};
+  var space2 = {front:'', left:'d', right:'h', floor:'g'};
+  var space3 = {front:'', left:'h', right:'d', floor:''};
+  var space4 = {front:'', left:'d', right:'h', floor:''};
+  var spaces = [space1, space2, space3, space4];
+  return (
+    RenderHall(spaces)
   );
 }
 
