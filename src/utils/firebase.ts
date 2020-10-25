9c58f1f5928a7c2ca266da09514d537006995cab
Bryan Pan <bryanpan342@gmail.com>
2020-10-25 11:37:58 -0500

    feat: basic get/update crud operations (#9)
    - **get**, takes in a reducer (setState) and then performs the getOperation to update on setState
    - **update**, takes in `UserInfo` and updates the user information with the props in `UserInfo` if none exist, _Firebase will default to the `this.user` properties

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,5 +1,8 @@
 import firebase from 'firebase';
 
+// import { INITIAL_LEVEL } from '../components/Levels';
+const INITIAL_LEVEL = 'level1';
+
 const config = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
@@ -12,26 +15,59 @@ const config = {
 };
 const app = firebase.initializeApp(config);
 
+/**
+ * The UserInfo that the app needs to process levels
+ */
 export interface UserInfo {
   name?: string,
   email?: string,
-  level?: number,
+  level?: string,
 }
 
+/**
+ * The props for the _Firebase Class
+ */
 export interface _FirebaseProps {
+  /**
+   * The hook for initializing the _Firebase.load()
+   *
+   * @param b the boolean value to change app state
+   */
   setIsSignedIn: (b: boolean) => void;
 }
 
+/**
+ * The class to perform operations on Firebase
+ */
 export class _Firebase {
-
+  /**
+   * the sign in hook
+   */
   public setIsSignedIn: (b: boolean) => void;
-  public user: UserInfo;
+  /**
+   * user info will be stored here
+   */
+  public user?: UserInfo;
+  /**
+   * the authenticated user information
+   *
+   * Used for the firebase CRUD operations
+   */
+  protected auth_user?: firebase.User;
 
   public constructor(){
     this.setIsSignedIn = (b) => {b;};
-    this.user = {};
   }
 
+  /**
+   * When component mounts, run the this function.
+   *
+   * 1. Set the persistence for the firebase.app to persist locally
+   * 2. Sign in the user and store the auth_user
+   * 3. Update this.user with through a POST
+   *
+   * @param props the properties to for load
+   */
   public load(props: _FirebaseProps): void  {
     this.setIsSignedIn = props.setIsSignedIn;
     this.user = {};
@@ -40,13 +76,17 @@ export class _Firebase {
     firebase.auth(app).onAuthStateChanged((user: firebase.User | null) => {
       if (user) {
         this.setIsSignedIn(true);
-        if (Object.keys(this.user).length === 0){
-          this.postUser(user);
+        this.auth_user = user;
+        if (this.user && Object.keys(this.user).length === 0){
+          void this.postUser();
         }
       }
     });
   }
 
+  /**
+   * @returns the UI Configuration for StyledFirebaseAuth
+   */
   public uiConfig(): firebaseui.auth.Config {
     return {
       signInFlow: 'popup',
@@ -63,34 +103,77 @@ export class _Firebase {
     };
   }
 
+  /**
+   * @returns the firebase.app
+   */
   public auth(): firebase.auth.Auth {
     return firebase.auth(app);
   }
 
+  /**
+   * Sign out and empty this.user
+   */
   public signOut(): void {
     void firebase.auth(app).signOut().then(() => this.user = {});
   }
 
-  public postUser(auth_user: firebase.User, setState?: (t: UserInfo) => void ): void {
-    const document = firebase.firestore(app).collection('users').doc(auth_user.uid);
-    void document.get().then((doc) => {
-      this.user = doc.exists ? doc.data() : this.putUser(auth_user);
-      if (setState) setState(this.user);
+  /**
+   * Retrieves the document on a user and perform an operation
+   *
+   * @param ternaryOp if the document doesnt exist, complete this operation
+   */
+  protected retrieveDocument(ternaryOp: any) {
+    if(!this.auth_user) return;
+    const document = firebase.firestore(app).collection('users').doc(this.auth_user.uid);
+    return document.get().then((doc) => {
+      return doc.exists ? doc.data() : ternaryOp;
     });
   }
 
-  public putUser(auth_user: firebase.User): UserInfo {
-    const collection = firebase.firestore(app).collection('users');
-    const profile = auth_user.providerData[0];
+  /**
+   * GET operation for the user.
+   */
+  public getUser(): Promise<any> | undefined {
+    return this.retrieveDocument({});
+  }
+
+  /**
+   * POST operation for the user.
+   */
+  public postUser(): Promise<any> | undefined {
+    return this.retrieveDocument(this.putUser());
+  }
+
+  /**
+   * UPDATE operation for the user.
+   *
+   * @param updates the updated user object, defaults to this.user if not passed in
+   */
+  public updateUser(updates: UserInfo): Promise<any> | undefined {
+    if(!this.auth_user) return;
+    const document = firebase.firestore(app).collection('users').doc(this.auth_user.uid);
+    const updatedUser: UserInfo = {
+      name: updates.name ?? this.user?.name,
+      email: updates.email ?? this.user?.email,
+      level: updates.level ?? this.user?.level,
+    };
+    return document.update(updatedUser);
+  }
+
+  /**
+   * PUT operation for the user.
+   */
+  public putUser(): UserInfo {
+    if(!this.auth_user) return {};
+    const document = firebase.firestore(app).collection('users').doc(this.auth_user.uid);
+    const profile = this.auth_user.providerData[0];
 
     const deets: UserInfo = {
       name: profile?.displayName ?? 'Anonymous User',
       email: profile?.email ?? 'N/A',
-      level: 0,
+      level: INITIAL_LEVEL,
     };
-    void collection.doc(auth_user.uid).set(deets);
+    void document.set(deets);
     return deets;
   }
 }
-
-
