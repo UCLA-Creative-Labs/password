d3852ba9462fea367ef76ff11c69189eabf95105
Bryan Pan <bryanpan342@gmail.com>
2020-10-29 14:30:20 -0500

    added some stuff for other browswers

--- src/components/Levels/Ghostie.tsx
+++ src/components/Levels/Ghostie.tsx
@@ -13,16 +13,49 @@ export default function Ghostie(): JSX.Element {
     }
   };
 
-  useEffect(() => {
-    if ('storage' in navigator && 'estimate' in navigator.storage) {
-      void navigator.storage.estimate().then(({usage, quota}) => {
-        if(quota && quota < 1200000000){
-          setIsIncognito(true);
+  function detectPrivateMode() {
+    var db,
+    on = () => setIsIncognito(true),
+    off = () => setIsIncognito(false)
+
+    function tryls() {
+        try {
+            localStorage.length ? off() : (localStorage.x = 1, localStorage.removeItem("x"), off());
+        } catch (e) {
+            // Safari only enables cookie in private mode
+            // if cookie is disabled then all client side storage is disabled
+            // if all client side storage is disabled, then there is no point
+            // in using private mode
+            navigator.cookieEnabled ? on() : off();
         }
-      });
-    } else {
-      setIsIncognito(true);
     }
+
+    function isChrome(){
+      return /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    }
+
+    function testChrome(){ 
+      if ('storage' in navigator && 'estimate' in navigator.storage) {
+        void navigator.storage.estimate().then(({usage, quota}) => {
+          if(quota && quota < 1200000000) on();
+        });
+      }
+    }
+    
+    // Blink (chrome & opera)
+    isChrome() ? testChrome()
+    // FF
+    : "MozAppearance" in document.documentElement.style ? (db = indexedDB.open("test"), db.onerror = on, db.onsuccess = off)
+    // Safari
+    : /constructor/i.test(window.HTMLElement) || window.safari ? tryls()
+    // IE10+ & edge
+    : !window.indexedDB && (window.PointerEvent || window.MSPointerEvent) ? on()
+    // Rest
+    : off();
+  }
+
+  useEffect(() => {
+    detectPrivateMode();
   });
 
   return (
