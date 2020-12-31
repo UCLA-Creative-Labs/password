aa6e713d76b7469c65fbf9eb0e75f9c620a34909
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2020-12-31 12:13:43 -0500

    Internet detection, WIP movement

--- src/components/Levels/NoInternet.tsx
+++ src/components/Levels/NoInternet.tsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 import Level from '../Levels';
 
+import '../styles/NoInternet.scss';
+
 export default function NoInternet(): JSX.Element {
   const [ isOnline, setIsOnline ] = useState(true);
+
+  const playerPos = useRef<[number, number]>([0, 0]);
 
   const handleOffline = () => {
     setIsOnline(false);
@@ -13,14 +17,47 @@ export default function NoInternet(): JSX.Element {
     setIsOnline(true);
   }
 
+  const handleKeypress = (e: KeyboardEvent) => {
+    const player = document.getElementById('player');
+    if (!player || !player.style || !isOnline) return;
+
+    switch (e.key) {
+      case 'w':
+      case 'ArrowUp':
+        if (playerPos.current[0] >= 0)
+          playerPos.current[0] -= 10;
+        break;
+      case 'a':
+      case 'ArrowLeft':
+        if (playerPos.current[1] >= 0)
+          playerPos.current[1] -= 10;
+        break;
+      case 's':
+      case 'ArrowDown':
+        if (playerPos.current[0] <= document.body.clientHeight - 100)
+          playerPos.current[0] += 10;
+        break;
+      case 'd':
+      case 'ArrowRight':
+        if (playerPos.current[1] <= document.body.clientWidth - 100)
+          playerPos.current[1] += 10;
+        break;
+      default:
+        break;
+    }
+    player.style.top = `${playerPos.current[0]}px`;
+    player.style.left = `${playerPos.current[1]}px`;
+  }
+
   useEffect(() => {
     window.addEventListener('offline', handleOffline);
-
     window.addEventListener('online', handleOnline);
+    window.addEventListener('keydown', handleKeypress);
 
     return () => {
       window.removeEventListener('offline', handleOffline);
       window.removeEventListener('online', handleOnline);
+      window.removeEventListener('keydown', handleKeypress);
     };
   }, []);
 
@@ -30,7 +67,15 @@ export default function NoInternet(): JSX.Element {
       nextLevelUrl=''
       isCompleted={false}
     >
-      <h2>{ isOnline ? 'Online' : 'Offline' }</h2>
+      <div
+        className={(isOnline ? 'on ' : 'off ') + 'base'}
+      >
+        <div
+          id={'player'}
+        >
+          O
+        </div>
+      </div>
     </Level>
   );
 }
